04a4f32add5de0809900a594fb93fad6628b1a95
Tom McClelland <tom@mcclelland.me>
2016-10-10 13:27:29 -0400

    madlib complete

---
+++ madlib.js
@@ -0,0 +1,25 @@
+var noun1 = 'slingshot'
+var noun2 = 'volleyball'
+var holiday1 = 'Pirate Day'
+var person1 = 'Mya Angeloo'
+var noun3 = 'Washington state'
+var noun4 = 'taco'
+var noun5 = 'Fitbit'
+var color1 = 'lime green'
+var vegetables1 = 'lettuce'
+var noun6 = 'asia'
+var verb1 = 'kick'
+var verb2 = 'punch'
+var exclamation1 = 'YOWSA'
+var vegetable1 = 'carrot'
+var noun7 = 'pig'
+var verb3 = 'poke'
+var verb4 = 'drown'
+var person2 =  'Peter Pan'
+var exclamation2 = 'sweet little baby jesus'
+var holiday2 = 'bring your dog to work day'
+var likesBeer = true
+
+var madlib = `Tonight's the ${noun1}! The one ${noun2} we all wait for, well next to ${holiday1}. ${person1} and I are going to be a ${noun3} and ${noun4}. The ${noun5} turns ${color1}. The ${vegetables1} are lit. We're ready to collect our ${noun6}! A quick ${verb1} on the door to ${verb2} what we get. ${exclamation1} it's a ${vegetable1}, my favorite I bet! More ${noun7} to ${verb3} upon, more treats to ${verb4}. ${person2} and I are finally done! ${exclamation2} for Halloween my favorite ${holiday2}! Halloween makes me tired, time for a ${(likesBeer ? 'Miller Lite' : 'Diet Coke')}.`
+
+console.log(madlib)
